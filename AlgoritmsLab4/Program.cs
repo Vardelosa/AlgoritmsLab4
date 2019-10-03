9f32f00ab71e7b4fdd8f7d78e54fa20a3789d623
vova chacov <vvvoha13@gmail.com>
2019-10-03 21:01:12 +0300

    Добавьте файлы проекта.

---
+++ AlgoritmsLab4/Program.cs
@@ -0,0 +1,41 @@
+﻿//!готово!-основні операції над статичною/динамічною чергою;
+//-змінити місцями перший та останній елемент черги;
+//-розвернути чергу;
+//-!готово!видалити всі елементи черги;
+//-!готово!операція «чи належить заданий елемент» статичній/динамічній черзі.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsLab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Kate");
+            queue.Enqueue("Sam");
+            queue.Enqueue("Alice");
+            queue.Enqueue("Tom");
+
+            foreach (string item in queue)
+                Console.WriteLine(item);
+            Console.WriteLine();
+            queue.Reverse();
+            foreach (string item in queue)
+                Console.WriteLine(item);
+            //Console.WriteLine();
+            //string firstItem = queue.Dequeue();
+            //Console.WriteLine($"Извлеченный элемент: {firstItem}");
+            //Console.WriteLine();
+
+            //foreach (string item in queue)
+            //    Console.WriteLine(item);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlgoritmsLab4/Queue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsLab4
+{
+    class Queue<T>:IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> tempNode = tail;
+            tail = node;
+            if (count == 0)
+                head = tail;
+            else
+                tempNode.Next = tail;
+            count++;
+        }
+        public T Dequeue()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = head.Data;
+            head = head.Next;
+            count--;
+            return output;
+        }
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        public void Reverse()
+        {
+            Node<T> current = tail;
+            Queue<T> temp = new Queue<T>();
+            while(current !=head)
+            {
+                temp.Enqueue(current.Data);
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+    }
+}
